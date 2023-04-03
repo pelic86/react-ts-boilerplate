553c738d97f22a574cd73d128fe55bc2bf54877c
Pelic Hsu <61898703+pelic86@users.noreply.github.com>
2023-04-03 19:10:16 +0800

    set up CRA + Typescript with prettier and eslint
    remove the useless default files from CRA, install prettier and eslint packages

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        Welcome to the starter of create react app with typescript.
+      </header>
+    </div>
+  );
+}
+
+export default App;
